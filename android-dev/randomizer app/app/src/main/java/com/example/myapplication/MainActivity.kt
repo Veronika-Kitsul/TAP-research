60ee2b4ae188c802809a5bd74929bf54524a3df5
Veronika Kitsul <65815639+Veronika-Kitsul@users.noreply.github.com>
2021-12-29 08:17:23 +0200

    some code

---
+++ android-dev/randomizer app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.button)
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+            val rand = Random().nextInt(seekBar.progress)
+            resultsTextView.text = rand.toString()
+        }
+    }
+}
---
+++ rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/veronikakitsul/Desktop/TAP-research/rust/hello_cargo/target/debug/hello_cargo: /Users/veronikakitsul/Desktop/TAP-research/rust/hello_cargo/src/main.rs
