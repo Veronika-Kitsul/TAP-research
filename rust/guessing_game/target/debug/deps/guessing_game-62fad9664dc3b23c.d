0ed7274decaf03b93a4ee8802e2403bf8cabb7d4
Kevin Liao <kevliao@mit.edu>
2022-01-12 11:18:56 -0500

    Add gitignore

--- rust/guessing_game/target/debug/deps/guessing_game-62fad9664dc3b23c.d
+++
@@ -1,5 +0,0 @@
-/Users/veronikakitsul/Desktop/TAP-research/rust/guessing_game/target/debug/deps/guessing_game-62fad9664dc3b23c: src/main.rs
-
-/Users/veronikakitsul/Desktop/TAP-research/rust/guessing_game/target/debug/deps/guessing_game-62fad9664dc3b23c.d: src/main.rs
-
-src/main.rs:
--- rust/hello_cargo/target/debug/deps/hello_cargo-25a0a8debb30444a.d
+++
@@ -1,5 +0,0 @@
-/Users/veronikakitsul/Desktop/TAP-research/rust/hello_cargo/target/debug/deps/hello_cargo-25a0a8debb30444a: src/main.rs
-
-/Users/veronikakitsul/Desktop/TAP-research/rust/hello_cargo/target/debug/deps/hello_cargo-25a0a8debb30444a.d: src/main.rs
-
-src/main.rs:
