37d17e3c89ba0d8d330af1ec4f77da1e4e9e38e3
Veronika Kitsul <65815639+Veronika-Kitsul@users.noreply.github.com>
2022-01-19 15:35:59 -0500

    rust code

--- rust/server/src/lib.rs
+++ rust/server/src/lib.rs
@@ -5,7 +5,7 @@ use std::sync::Mutex;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 enum Message {
@@ -38,7 +38,7 @@ impl ThreadPool {
         {
             let job = Box::new(f);
 
-            self.sender.send(job).unwrap();
+            self.sender.send(Message::NewJob(job)).unwrap();
         }
 }
 
@@ -60,16 +60,28 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-            println!("Worker {} got a job; executing.", id);
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
 
-            job();
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+
+                    break;
+                }
+            }
+
+            
         });
 
-        Worker {id, 
+        Worker {
+                id, 
                 thread: Some(thread), 
             }
     }
