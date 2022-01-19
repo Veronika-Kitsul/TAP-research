de432c2f842f5b9b0b8a018e56b1c8f99c1d6bf2
Kevin Liao <kevliao@mit.edu>
2022-01-19 16:01:45 -0500

    Add gitignore

--- rust/server/target/debug/deps/main-bb83655fa987738a.d
+++
@@ -1,5 +0,0 @@
-/Users/veronikakitsul/Desktop/TAP-research/rust/server/target/debug/deps/main-bb83655fa987738a.rmeta: src/bin/main.rs
-
-/Users/veronikakitsul/Desktop/TAP-research/rust/server/target/debug/deps/main-bb83655fa987738a.d: src/bin/main.rs
-
-src/bin/main.rs:
--- rust/server/target/debug/deps/server-1f4b5eedd708da3e.d
+++
@@ -1,5 +0,0 @@
-/Users/veronikakitsul/Desktop/TAP-research/rust/server/target/debug/deps/server-1f4b5eedd708da3e.rmeta: src/lib.rs
-
-/Users/veronikakitsul/Desktop/TAP-research/rust/server/target/debug/deps/server-1f4b5eedd708da3e.d: src/lib.rs
-
-src/lib.rs:
