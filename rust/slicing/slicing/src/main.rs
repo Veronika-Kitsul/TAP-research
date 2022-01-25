2d30b4b07a31f9d912a5b92561b064b366b2ede7
Veronika Kitsul <65815639+Veronika-Kitsul@users.noreply.github.com>
2022-01-24 20:37:43 -0500

    more code

---
+++ rust/slicing/slicing/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let s = "Hello, world"
+    first_word(&s);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b'' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
