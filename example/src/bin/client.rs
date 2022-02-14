e97dcdcafa894236870ddf1d2789b12fce4fe56c
Veronika Kitsul <65815639+Veronika-Kitsul@users.noreply.github.com>
2022-02-13 20:40:08 -0500

    encryption successful

--- example/src/bin/client.rs
+++ example/src/bin/client.rs
@@ -1,44 +1,82 @@
-extern crate openssl;
-
 use std::io::Write;
 use std::net::TcpStream;
 use example::format::{Message,MessageType};
-use serde::{Serialize, Deserialize};
 use bincode;
-use openssl::rsa::{Rsa, Padding};
-use openssl::symm::Cipher;
+use rand::prelude::*;
+use hpke::{
+    aead::{AeadTag, ChaCha20Poly1305},
+    kdf::HkdfSha384,
+    kem::X25519HkdfSha256,
+    Deserializable, Kem as KemTrait, OpModeR, OpModeS, Serializable,
+};
+
+type Kem = X25519HkdfSha256;
+type Aead = ChaCha20Poly1305;
+type Kdf = HkdfSha384;
+
+const INFO_STR: &[u8] = b"example session";
+
+// initialize the server with a key pair
+fn server_init() -> (<Kem as KemTrait>::PrivateKey, <Kem as KemTrait>::PublicKey) {
+    let mut csprng = StdRng::from_entropy();
+    Kem::gen_keypair(&mut csprng)
+}
+
+fn encrypt_msg(
+    msg: &[u8],
+    aad: &[u8],
+    server_pk: &<Kem as KemTrait>::PublicKey,) -> (<Kem as KemTrait>::EncappedKey, Vec<u8>, AeadTag<Aead>) {
+        let mut csprng = StdRng::from_entropy();
+
+        // encapsulate a key and use the resulting shared secret to encrypt a message
+        // encrypt with AEAD context
+        let (encapsulated_key, mut sender_ctx) = hpke::setup_sender::<Aead, Kdf, Kem, _>
+                                                    (&OpModeS::Base, &server_pk, INFO_STR, 
+                                                    &mut csprng).expect("invalid server pubkey!");
+        // seal in place will encrypt the plaintext in place if success
+        let mut msg_copy = msg.to_vec();
+        let tag = sender_ctx.seal_in_place_detached(&mut msg_copy, aad).expect("encryption failed!");
+
+        let ciphertext = msg_copy;
+        println!("ciphertext: {:?}", ciphertext);
+        // return
+        (encapsulated_key, ciphertext, tag)
+    }
+
 
 fn main() {
-    // Create a message
+    // set up the server 
+    let (server_privkey, server_pubkey) = server_init();
+
+     // Create a message
     let msg = Message {
         msgtype: MessageType::Type1,
         contents: b"Hello".to_vec(),
     };
 
-    // Marshall the message into byte string
-    // Using the serde crate, probably marshal into bincode
-    let serialized = bincode::serialize(&msg).unwrap();
+    let aad = b"First encrypted message";
 
+    // Marshall the message into bincode
+    let serialized: Vec<u8> = bincode::serialize(&msg).unwrap();
+    // looks good
+    println!("serialized: {:?}",serialized);
 
-    // Encrypt the message into a ciphertext
-    // Use public key encryption
-    let encrypted = b"Hello";
-  
+    let (encapsulated_key, ciphertext, tag) = encrypt_msg(&serialized, aad, &server_pubkey);
+    let encapsulated_key_bytes = encapsulated_key.to_bytes();
+    let tag_bytes = tag.to_bytes();
+
     // Open a connection to the server
-    match TcpStream::connect("") {
+    match TcpStream::connect("127.0.0.1") {
 
         Ok(mut stream) => {
             println!("Connected to the server!");
 
             // Send ciphertext to the server
-            stream.write(encrypted).unwrap();
+            stream.write(&ciphertext).unwrap();
         }
         
         Err(e) => {
             println!("There was an error.");
         }
     }
-
-    // Gets a response from server
-    // how do i receive a message
 }
--- example/src/bin/server.rs
+++ example/src/bin/server.rs
@@ -2,13 +2,15 @@ use std::net::TcpListener;
 use std::thread;
 
 fn main() {
+    let listener = TcpListener::bind("127.0.0.1").unwrap();
 
-    let listener = TcpListener::bind("").unwrap();
+    //pass key
 
     // Listen for connections on a loop
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
+        // how many threads -- 4
         thread::spawn(move|| {
             // decrypt a message with a key
 
