ec04ada0549f9a2b1daad5aa8b25d8192195780c
kev-liao <kevliao@mit.edu>
2022-04-06 16:18:41 -0400

    keygen

--- example/src/bin/key.rs
+++ example/src/bin/key.rs
@@ -1,7 +1,15 @@
-extern crate openssl;
+//extern crate openssl;
+//use openssl::rsa::{Rsa, Padding};
+//use openssl::symm::Cipher;
+use hpke::{
+    kem::X25519HkdfSha256,
+    Kem as KemTrait,
+    Serializable,
+};
+use rand::{rngs::StdRng, SeedableRng};
+use std::{fs::File, io::Write};
 
-use openssl::rsa::{Rsa, Padding};
-use openssl::symm::Cipher;
+type Kem = X25519HkdfSha256;
 
 fn server_init() -> (<Kem as KemTrait>::PrivateKey, <Kem as KemTrait>::PublicKey) {
     // is this good? or the other one is better? should I write this into a file instead?
@@ -15,14 +23,16 @@ fn server_init() -> (<Kem as KemTrait>::PrivateKey, <Kem as KemTrait>::PublicKey
 }
 
 fn main() {
-    let passphrase = "generating-keys";
-    let private_key, public_key = server_init();
+    let (private_key, public_key) = server_init();
 
-    println!("Private key: {}", String::from_utf8(private_key).unwrap());
-    println!("Public key: {}", String::from_utf8(public_key).unwrap());
+    println!("{:?}", private_key.to_bytes());
+    println!("{:?}", public_key.to_bytes());
 
-    let mut file = File::create("private.txt")?;
-    file.write_all(private_key)?;
-    let mut file = File::create("public.txt")?;
-    file.write_all(public_key)?;
+    //println!("Private key: {}", String::from_utf8(private_key).unwrap());
+    //println!("Public key: {}", String::from_utf8(public_key).unwrap());
+
+    //let mut file = File::create("private.txt")?;
+    //file.write_all(private_key)?;
+    //let mut file = File::create("public.txt")?;
+    //file.write_all(public_key)?;
 }
