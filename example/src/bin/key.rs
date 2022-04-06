8b197d71703749bc4a9e2d155fbfd4b15e2e8338
Veronika Kitsul <65815639+Veronika-Kitsul@users.noreply.github.com>
2022-04-06 16:05:55 -0400

    changes to the code

--- example/src/bin/key.rs
+++ example/src/bin/key.rs
@@ -3,20 +3,26 @@ extern crate openssl;
 use openssl::rsa::{Rsa, Padding};
 use openssl::symm::Cipher;
 
+fn server_init() -> (<Kem as KemTrait>::PrivateKey, <Kem as KemTrait>::PublicKey) {
+    // is this good? or the other one is better? should I write this into a file instead?
+    let mut csprng = StdRng::from_entropy();
+
+    // let mut file = File::create("private.txt");
+    // file.write_all(Kem::pubkey);
+    // let mut file = File::create("public.txt");
+    // file.write_all(public_key);
+    Kem::gen_keypair(&mut csprng)
+}
+
 fn main() {
     let passphrase = "generating-keys";
-
-    let rsa = Rsa::generate(1024).unwrap();
-    let private_key: Vec<u8> = rsa.private_key_to_pem_passphrase(Cipher::aes_128_cbc(), passphrase.as_bytes()).unwrap();
-    let public_key: Vec<u8> = rsa.public_key_to_pem().unwrap();
+    let private_key, public_key = server_init();
 
     println!("Private key: {}", String::from_utf8(private_key).unwrap());
     println!("Public key: {}", String::from_utf8(public_key).unwrap());
-    // the problem is my key is the same as in the example and I don't know why
 
     let mut file = File::create("private.txt")?;
     file.write_all(private_key)?;
     let mut file = File::create("public.txt")?;
     file.write_all(public_key)?;
-    // how do i nicely store both keys? separate files?
 }
