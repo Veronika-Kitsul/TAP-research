b2bbe81793df1c764be0cbc87824dd97bab73155
kev-liao <kevliao@mit.edu>
2022-04-06 16:45:51 -0400

    Keygen

--- example/src/bin/key.rs
+++ example/src/bin/key.rs
@@ -1,10 +1,11 @@
-//extern crate openssl;
-//use openssl::rsa::{Rsa, Padding};
-//use openssl::symm::Cipher;
+use std::io;
+use std::io::Read;
+use std::io::BufReader;
 use hpke::{
     kem::X25519HkdfSha256,
     Kem as KemTrait,
     Serializable,
+    Deserializable,
 };
 use rand::{rngs::StdRng, SeedableRng};
 use std::{fs::File, io::Write};
@@ -21,11 +22,21 @@ fn server_init() -> (<Kem as KemTrait>::PrivateKey, <Kem as KemTrait>::PublicKey
 fn main() {
     let (private_key, public_key) = server_init();
 
-    //println!("{:?}", private_key.to_bytes());
+    println!("{:?}", private_key.to_bytes());
     //println!("{:?}", public_key.to_bytes());
 
     let mut priv_file = File::create("private.txt").unwrap();
-    priv_file.write_all(private_key.to_bytes().as_slice());
+    priv_file.write_all(private_key.to_bytes().as_slice()).unwrap();
     let mut pub_file = File::create("public.txt").unwrap();
-    pub_file.write_all(public_key.to_bytes().as_slice());
+    pub_file.write_all(public_key.to_bytes().as_slice()).unwrap();
+
+    let f = File::open("private.txt").unwrap();
+    let mut reader = BufReader::new(f);
+    let mut priv_key_bytes = Vec::new();
+    
+    // Read file into vector.
+    reader.read_to_end(&mut priv_key_bytes).unwrap();
+
+    let private_key2: <Kem as KemTrait>::PrivateKey =
+        Deserializable::from_bytes(&priv_key_bytes).unwrap();
 }
